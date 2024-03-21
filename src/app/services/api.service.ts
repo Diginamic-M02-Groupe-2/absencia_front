2c617181e3fff2b87afb28ee8f4ac55d171c4029
Clement Olivier <oliclems@gmail.com>
2024-03-21 12:22:14 +0100

    fix(route): fix nommage route api get user

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,4 @@
 const API_URL = 'http://localhost:8080/api';
 export const AUTH_API = `${API_URL}/login`;
 export const LOGOUT_API = `${API_URL}/logout`;
-export const GET_USER_API = `${API_URL}/user`;
+export const GET_USER_API = `${API_URL}/current-user`;
