7fd1240a493d7e43b1c36674ffce305a9e4a9b8a
matteokeole <matteo@keole.net>
2024-03-22 17:12:09 +0100

    feat: Badge component

--- src/app/pages/components/components.module.ts
+++ src/app/pages/components/components.module.ts
@@ -5,9 +5,11 @@ import {DropdownModule} from "primeng/dropdown";
 import {InputTextModule} from "primeng/inputtext";
 import {SelectButtonModule} from "primeng/selectbutton";
 import {ComponentsComponent} from "./components.component";
+import {BadgeComponent} from "../../components/badge/badge.component";
 
 @NgModule({
   declarations: [
+    BadgeComponent,
     ComponentsComponent,
   ],
   imports: [
