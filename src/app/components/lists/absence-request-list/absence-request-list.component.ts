4756602563cc86c4bce4e980384c79783f76cf44
Clement Olivier <oliclems@gmail.com>
2024-03-26 12:09:28 +0100

    feat(deleteRequestAbsence): delete ok

--- src/app/components/lists/absence-request-list/absence-request-list.component.ts
+++ src/app/components/lists/absence-request-list/absence-request-list.component.ts
@@ -1,27 +1,69 @@
 import { Component } from '@angular/core';
 import { AbsenceRequest } from '../../../models/absence-request';
-import { Observable } from 'rxjs';
+import { catchError, firstValueFrom, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { GET_ABSENCE_REQUESTS } from '../../../services/api.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-absence-request-list',
   templateUrl: './absence-request-list.component.html',
-  styleUrl: './absence-request-list.component.scss'
+  styleUrl: './absence-request-list.component.scss',
+  providers: [MessageService],
 })
 export class AbsenceRequestListComponent {
-
-  absenceRequests$: Observable<Array<AbsenceRequest>> | undefined;
-  currentUser = JSON.parse(sessionStorage.getItem('currentUser')!)
-
-  constructor(private http: HttpClient) { }
+  currentUser = JSON.parse(sessionStorage.getItem('currentUser')!);
+  absenceRequests: AbsenceRequest[] = [];
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
-    this.absenceRequests$ = this.getAbsenceRequests();
+    this.getAbsenceRequests();
   }
 
-  getAbsenceRequests(): Observable<Array<AbsenceRequest>> {
-    return this.http.get<Array<AbsenceRequest>>(GET_ABSENCE_REQUESTS + "/"+ this.currentUser.id)
+  async getAbsenceRequests(): Promise<void> {
+    this.absenceRequests = await firstValueFrom(
+      this.http.get<Array<AbsenceRequest>>(
+        GET_ABSENCE_REQUESTS + '/' + this.currentUser.id
+      )
+    );
   }
 
+  async deleteAbsenceRequest(absenceRequest: AbsenceRequest) {
+    const deletedAbsenceRequest = this.http
+      .delete<any>(`${GET_ABSENCE_REQUESTS}/${absenceRequest.id}`)
+      .pipe(
+        catchError((error) => {
+          if (error.status === 400 && error.error && error.error.message) {
+            return throwError(() => new Error(error.error.message));
+          } else {
+            return throwError(() => error);
+          }
+        })
+      );
+
+    deletedAbsenceRequest
+      .pipe(
+        catchError((error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Suppression',
+            detail: error,
+            life: 5000,
+          });
+          return throwError(() => error);
+        })
+      )
+      .subscribe((response: { message: string }) => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Suppression',
+          detail: response.message,
+          life: 5000,
+        });
+        this.getAbsenceRequests();
+      });
+  }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,20 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { AbsenceRequestListComponent } from '../components/lists/absence-request-list/absence-request-list.component';
 import { TableModule } from 'primeng/table';
-import { AbsenceTypePipe } from "../pipes/absence-type.pipe";
+import { AbsenceTypePipe } from '../pipes/absence-type.pipe';
 import { AbsenceRequestStatusPipe } from '../pipes/absence-request-status.pipe';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [DashboardComponent, AbsenceRequestListComponent],
-  imports: [CommonModule, TableModule, AbsenceTypePipe, AbsenceRequestStatusPipe]
+  imports: [
+    CommonModule,
+    TableModule,
+    AbsenceTypePipe,
+    AbsenceRequestStatusPipe,
+    ButtonModule,
+    ToastModule
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
