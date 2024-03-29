9703c8302abd75a9a745686ac7b697cbfe22b696
pamelacalcei <pecalcei@gmail.com>
2024-03-29 10:12:18 +0100

    remove User in absencerequest file

--- src/app/entities/absence-request.ts
+++ src/app/entities/absence-request.ts
@@ -1,10 +1,8 @@
 import {AbsenceRequestStatus} from "./absence-request-status";
 import {AbsenceType} from "./absence-type";
-import { User } from "./user/user";
 
 export class AbsenceRequest {
   id!: number;
-  user?: User;
   type!: AbsenceType;
   startedAt!: Date;
   endedAt!: Date;
