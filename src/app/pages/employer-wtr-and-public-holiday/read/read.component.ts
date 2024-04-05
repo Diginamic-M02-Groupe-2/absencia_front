aaa2557a93fe0f92c1ac6c11e1e8f851427fe8a6
matteokeole <matteo@keole.net>
2024-04-05 17:09:39 +0200

    feat: Filter employer WTR/public holidays by year

--- src/app/pages/employer-wtr-and-public-holiday/read/read.component.ts
+++ src/app/pages/employer-wtr-and-public-holiday/read/read.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {FormBuilder, FormGroup} from "@angular/forms";
 import {firstValueFrom} from "rxjs";
 import {EmployerWtr} from "../../../entities/employer-wtr";
 import {PublicHoliday} from "../../../entities/public-holiday";
@@ -12,20 +13,27 @@ import {ApiRoute, ApiService} from "../../../services/api.service";
   styleUrl: "./read.component.module.scss",
 })
 export class EmployerWtrAndPublicHolidayReadComponent {
+  formGroup: FormGroup;
+
   createEmployerWtrRoute: string = `/${Route.EMPLOYER_WTR_CREATE}`;
 
   employerWtrAndPublicHolidays: (EmployerWtr|PublicHoliday)[] = [];
 
-  year: number = new Date().getFullYear();
-
   constructor(
+    private formBuilder: FormBuilder,
     private apiService: ApiService,
   ) {
+    this.formGroup = this.formBuilder.group({
+      year: [new Date(new Date().getFullYear().toString())],
+    });
+
+    this.formGroup.get("year")!.valueChanges.subscribe((value: Date) => this.getEmployerWtrAndPublicHolidays());
+
     this.getEmployerWtrAndPublicHolidays();
   }
 
   async getEmployerWtrAndPublicHolidays(): Promise<void> {
-    const url = `${ApiRoute.GET_EMPLOYER_WTR_AND_PUBLIC_HOLIDAYS}/${this.year}`;
+    const url = `${ApiRoute.GET_EMPLOYER_WTR_AND_PUBLIC_HOLIDAYS}/${this.formGroup.get("year")!.value.getFullYear()}`;
     const response = await firstValueFrom(this.apiService.get<GetEmployerWtrAndPublicHolidaysResponse>(url));
 
     this.employerWtrAndPublicHolidays = [
